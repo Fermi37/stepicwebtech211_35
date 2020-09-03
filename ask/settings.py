9c23822a0441417df986a2635bc99f3da6115f5a
Yuri Bulavin <nebalama@gmail.com>
2020-09-03 18:21:20 +0300

    base_dir os implement

--- ask/settings.py
+++ ask/settings.py
@@ -13,8 +13,10 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.abspath(__file__)
+BASE_DIR = os.path.dirname(BASE_DIR)
+BASE_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
