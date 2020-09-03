40058302897bdfcc5df6a1b8698fe2a8e74102c4
Yuri Bulavin <nebalama@gmail.com>
2020-09-03 22:30:15 +0300

    fix install_python

--- install_python.sh
+++ install_python.sh
@@ -9,8 +9,3 @@ sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 #sudo pip3 install --upgrade pip
 #sudo pip3 install --upgrade gunicorn
 
-virtualenv -p python3 myvenv
-source myvenv/bin/activate
-pip install -U pip
-pip install Django==2.1
-pip install gunicorn
