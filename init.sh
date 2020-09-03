a5e49beba981219e8c89a2836dd88464f9ec3a12
Yuri Bulavin <nebalama@gmail.com>
2020-09-03 18:11:53 +0300

    no message

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/nginx/stepic211_prod.conf  /etc/nginx/sites-enabled/stepic211_prod.conf
 sudo /etc/init.d/nginx restart
-sudo gunicorn --config=./hello_wsgi/hello_gunicorn_conf.py hello_wsgi.hello_app:app
-sudo gunicorn --config=./ask/ask_gunicorn_conf.py ask.wsgi:application
+sudo  gunicorn --config=./hello_wsgi/hello_gunicorn_conf.py hello_wsgi.hello_app:app &
+sudo  gunicorn --config=./ask/ask_gunicorn_conf.py ask.wsgi:application
