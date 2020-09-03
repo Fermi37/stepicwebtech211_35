362a6ed3817454135c94a4912034c5fccfb225c3
Yuri Bulavin <nebalama@gmail.com>
2020-09-03 21:39:43 +0300

    no message

--- install_python.sh
+++ install_python.sh
@@ -2,7 +2,8 @@ sudo apt-get update
 sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install python3.5
+sudo apt install -y python3.5
+sudo apt-get install -y python3.5-dev
 sudo unlink /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 sudo pip3 install --upgrade pip
