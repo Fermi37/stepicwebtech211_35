2a6d01da99dbe91e086b61e34fe9704f8ecac8c3
Yuri Bulavin <nebalama@gmail.com>
2020-09-03 21:59:06 +0300

    initial

--- ask/settings.py
+++ ask/settings.py
@@ -10,13 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-from pathlib import Path
+# from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-# BASE_DIR = os.path.abspath(__file__)
-# BASE_DIR = os.path.dirname(BASE_DIR)
-# BASE_DIR = os.path.dirname(BASE_DIR)
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.abspath(__file__)
+BASE_DIR = os.path.dirname(BASE_DIR)
+BASE_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
