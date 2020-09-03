7c34b267db511647682874144da7891b1c095b99
Yuri Bulavin <nebalama@gmail.com>
2020-09-03 19:17:21 +0300

    no message

--- install_python.sh
+++ install_python.sh
@@ -2,9 +2,9 @@ sudo apt-get update
 sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install -y python3.8
+sudo apt install python3.5
 sudo unlink /usr/bin/python3
-sudo ln -s /usr/bin/python3.8 /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 sudo python3 pip install --upgrade pip
 sudo python3 pip install --upgrade django
 sudo python3 pip install --upgrade gunicorn
